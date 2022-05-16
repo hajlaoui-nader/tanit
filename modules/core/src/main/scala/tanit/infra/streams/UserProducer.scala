cc67e8b0649029b4591f02c000c0c4c04696f152
hajlaoui-nader <hajlaoui.nader@gmail.com>
2022-05-16 10:27:15 +0200

    add producer

---
+++ modules/core/src/main/scala/tanit/infra/streams/UserProducer.scala
@@ -0,0 +1,27 @@
+package tanit.infra.streams
+
+import cats.effect.kernel.Async
+import tanit.infra.config.KafkaConfig
+import fs2.kafka.ProducerSettings
+import fs2.kafka.KafkaProducer
+import java.util.UUID
+import fs2.kafka.ProducerRecord
+import fs2.kafka.ProducerRecords
+
+class UserProducer[F[_]: Async](kafkaConfig: KafkaConfig, topic: String) {
+
+  private val producerSettings =
+    ProducerSettings[F, String, String]
+      .withBootstrapServers(kafkaConfig.bootstrapServers.mkString(","))
+
+  def produce =
+    KafkaProducer
+      .stream(producerSettings)
+      .evalMap { producer =>
+        val key    = UUID.randomUUID().toString
+        val value  = UUID.randomUUID().toString
+        val record = ProducerRecord(topic, key, value)
+        producer.produce(ProducerRecords.one(record))
+      }
+
+}

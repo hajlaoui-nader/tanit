3da81c3d9dc67b2fc1481c070d83dfb0c99f83b3
hajlaoui-nader <hajlaoui.nader@gmail.com>
2022-06-15 10:02:46 +0200

    healthroute test

--- modules/tests/src/test/scala/tanit/infra/api/HealthRouteSuite.scala
+++ modules/tests/src/test/scala/tanit/infra/api/HealthRouteSuite.scala
@@ -2,10 +2,24 @@ package tanit.infra.api
 
 import cats.effect._
 import weaver._
+import org.http4s.Method._
+import org.http4s._
+import org.http4s.client.dsl.io._
+import org.http4s.syntax.literals._
+// import io.circe._
+// import io.circe.syntax._
+// import org.http4s.circe._
 
 // TODO [nh] tests
 object HealthRouteSuite extends SimpleIOSuite {
   test("GET health status") {
-    IO { expect.same(1, 1) }
+    val req   = GET(uri"/healthcheck")
+    val route = new HealthRoute[IO].routes
+
+    route.run(req).value.flatMap {
+      case Some(resp) =>
+        IO { expect.same(resp.status, Status.Ok) }
+      case None => IO.pure(failure("route nout found"))
+    }
   }
 }

5edd6e16b7e4696317fe74ae92b50c25f1b76988
hajlaoui-nader <hajlaoui.nader@gmail.com>
2022-06-17 10:52:02 +0200

    add users implem

--- modules/core/src/main/scala/tanit/domain/UserService.scala
+++ modules/core/src/main/scala/tanit/domain/UserService.scala
@@ -1,7 +1,16 @@
 package tanit.domain
 
 import tanit.domain.data._
+import cats.effect.kernel.Sync
+
 trait Users[F[_]] {
   def create(user: User): F[User]
   def find(id: UserId): F[Option[User]]
+}
+
+final class UserService[F[_]: Sync]() extends Users[F] {
+  override def create(user: User): F[User] = ???
+
+  override def find(id: UserId): F[Option[User]] = ???
+
 }

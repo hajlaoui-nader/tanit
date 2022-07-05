fa9ec828a500fc1e0e4635771d35b00bce0115dd
hajlaoui-nader <hajlaoui.nader@gmail.com>
2022-07-05 09:12:58 +0200

    implement repo

--- modules/core/src/main/scala/tanit/infra/repository/OpensearchUsers.scala
+++ modules/core/src/main/scala/tanit/infra/repository/OpensearchUsers.scala
@@ -5,6 +5,9 @@ import tanit.domain.data
 
 // TODO [NH] implement me
 class OpensearchUsers[F[_]] extends Users[F] {
+
+  // val client = ElasticClient()
+
   override def create(user: data.User): F[data.User] = ???
 
   override def find(id: data.UserId): F[Option[data.User]] = ???

9f8942486c8558dcdd9bd47d5f5aeb63480e3598
hajlaoui-nader <hajlaoui.nader@gmail.com>
2022-07-20 10:55:52 +0200

    wip

--- modules/core/src/main/scala/tanit/infra/repository/OpensearchUsers.scala
+++ modules/core/src/main/scala/tanit/infra/repository/OpensearchUsers.scala
@@ -9,7 +9,7 @@ import com.sksamuel.elastic4s.ElasticClient
 // TODO [NH] implement me
 class OpensearchUsers[F[_]](config: OpensearchConfig) extends Users[F] {
 
-  val props  = ElasticProperties("http://host1:9200")
+  val props = ElasticProperties("http://host1:9200")
   // val client = ElasticClient(JavaClient(props))
 
   override def create(user: data.User): F[data.User] = ???

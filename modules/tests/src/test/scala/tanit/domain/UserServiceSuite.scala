23a000400ed5adf29a90f10da7ee85754aff704e
hajlaoui-nader <hajlaoui.nader@gmail.com>
2022-09-09 10:21:11 +0200

    test placeholder

--- modules/tests/src/test/scala/tanit/domain/UserServiceSuite.scala
+++ modules/tests/src/test/scala/tanit/domain/UserServiceSuite.scala
@@ -1,5 +1,13 @@
 package tanit.domain
 
 import weaver._
+import cats.effect.IO
 
-object UserServiceSuite extends SimpleIOSuite {}
+object UserServiceSuite extends SimpleIOSuite {
+  test("save user") {
+    IO {
+      println("testing")
+      expect.same(1, 1)
+    }
+  }
+}
